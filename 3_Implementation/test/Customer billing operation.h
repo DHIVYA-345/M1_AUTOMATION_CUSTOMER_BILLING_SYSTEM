d92ba5d77b19e27cb527ce8cb3019477ba88229c
DHIVYA-345 <94309132+DHIVYA-345@users.noreply.github.com>
2021-11-24 11:31:58 +0530

    updated unit testing

--- 3_Implementation/test/Customer billing operation.h
+++ 3_Implementation/test/Customer billing operation.h
@@ -6,7 +6,11 @@
 #define PROJECT_NAME    "Customer billing system"
 
 /* Prototypes for all the test functions */
-
+void create_customer();
+void display();
+void edit_customer();
+void create_item();
+void update_item();
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -20,43 +24,43 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+  RUN_TEST(test_create_customer);
+  RUN_TEST(test_display);
+  RUN_TEST(test_edit_customer);
+  RUN_TEST(test_create_item);
+  RUN_TEST(test_update_item);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
+void test_create_customer(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
   TEST_ASSERT_EQUAL(-10, add(10, -20));
   TEST_ASSERT_EQUAL(-30, add(-10, -20));
   TEST_ASSERT_EQUAL(10, add(-10, 20));
 }
-void test_add_testcase2(void) {
+void test_display(void) {
  
   /* Dummy fail*/
   // TEST_ASSERT_EQUAL(1500, add(750, 7500));
 }
-void test_subtract(void) {
+void test_edit_customer(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
   /* Dummy fail*/
   // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
 }
 
-void test_multiply(void) {
+void test_create_item(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
   
   /* Dummy fail*/
   // TEST_ASSERT_EQUAL(2, multiply(2, 5));
 }
 
-void test_divide(void) {
+void test_update_item(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
   
   /* Dummy fail*/
