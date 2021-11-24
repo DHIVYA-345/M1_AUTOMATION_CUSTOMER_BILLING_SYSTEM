b3527a49a66658805409374a88fd4efdd1f58d2d
DHIVYA-345 <94309132+DHIVYA-345@users.noreply.github.com>
2021-11-24 23:45:33 +0530

    Update Customer billing operation.h

--- 3_Implementation/test/Customer billing operation.h
+++ 3_Implementation/test/Customer billing operation.h
@@ -34,31 +34,47 @@ int main()
   return UNITY_END();
 }
 
-/* Write all the test functions */ 
-void test_create_customer(void) {
-
-void test_display(void) {
- 
-  /* Dummy fail*/
-  
-}
-void test_edit_customer(void) {
-  
-  
-  /* Dummy fail*/
-  
+void test_total_bill(void){
+    float qty1 = 5, qty2 = 5, price1 = 27, price2 = 5;
+    float ans1 = 135, ans2 = 25;
+    TEST_ASSERT_EQUAL(ans1, total_bill(qty1,price1));
+    TEST_ASSERT_EQUAL(25, total_bill(qty2,price2));
 }
 
-void test_create_item(void) {
-  
-  
-  /* Dummy fail*/
-  
+void test_net_total(void){
+    float num1 = 40,num2 = 80,num3 = 135;
+    float ans1 = 36,ans2 = 72,ans3 = 121.5;
+    TEST_ASSERT_EQUAL(ans1, net_total(num1));
+    TEST_ASSERT_EQUAL(ans2, net_total(num2));
+    TEST_ASSERT_EQUAL(ans3, net_total(num3));
 }
 
-void test_update_item(void) {
-  
-  
-  /* Dummy fail*/
-  
+void test_grand_total(void){
+    float input1 = 80, input2 = 140, input3 = 121.5;
+    float ans1 = 94.4, ans2 = 165.2, ans3 = 143.37; 
+    TEST_ASSERT_EQUAL(ans1, grand_total(input1));
+    TEST_ASSERT_EQUAL(ans2, grand_total(input2));
+    TEST_ASSERT_EQUAL(ans3, grand_total(input3));
 }
+void test_negative_total(void){
+  float qty1 = -5, qty2 = -32000, qty3 = 25, qty4 = -5, price1 = -76, price2 = -7600, price3 = -63,price4 = 34 ;
+  float ans = 0;
+  TEST_ASSERT_EQUAL(ans, total_bill(qty1,price1));
+  TEST_ASSERT_EQUAL(ans, total_bill(qty2,price2));
+  TEST_ASSERT_EQUAL(ans, total_bill(qty3,price3));
+  TEST_ASSERT_EQUAL(ans, total_bill(qty4,price4));
+}
+void test_negative_net_total(void){
+  float input1 = -32000, input2 = -1;
+  float ans =0;
+  TEST_ASSERT_EQUAL(0, net_total(-32000));
+  TEST_ASSERT_EQUAL(0, net_total(-1));
+}
+
+void test_negative_grand_total(void){
+  float input1 = -32000, input2 = -1;
+  float ans = 0;
+  TEST_ASSERT_EQUAL(0, grand_total(-32000));
+  TEST_ASSERT_EQUAL(0, grand_total(-1));
+}
+© 2021 GitHub, Inc.
